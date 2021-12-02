f5ec9aad1e2b54000f4395d96bf08b8b46485043
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2021-12-03 00:13:44 +0200

    Login screen added

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
-from django.contrib.auth.models import User
 
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from base.models import Product
+from base.products import products
+from base.serializers import ProductSerializer
 # Create your views here.
 
- 
-from django.contrib.auth.hashers import make_password
 from rest_framework import status
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+ 
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Container>
           <Routes>
             <Route path = '/' element={<HomeScreen/>} exact/>
+            <Route path = '/login' element={<LoginScreen/>} />
             <Route path = '/product/:id' element={<ProductScreen/>} exact />
             <Route path = '/cart/*' element={<CartScreen/>} />
             </Routes>
